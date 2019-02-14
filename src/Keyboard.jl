8f324057afa6161d81bdf0047a9a77ac8b4b9f09
bsxfan <bsxfan@gmail.com>
2019-02-14 15:23:17 +0200

    first commit

---
+++ src/Keyboard.jl
@@ -0,0 +1,44 @@
+module Keyboard
+
+import Base.@locals
+
+export keyboard
+
+
+function keyboard(dict,file,line)
+    println("keyboard at line ",line," in ",file)
+    label = splitpath(file)[end] * "@$(line)" 
+    while true
+        print(label,"] > ")
+        name = readline()
+        if isempty(name)
+            break
+        end
+
+        symbol = Meta.parse(name)
+        if !haskey(dict,symbol)
+            println("no local variable ",name)
+            display(dict)
+        else
+            print(name,"=")
+            display(dict[symbol])
+        end
+    end
+end
+
+
+
+
+
+
+function test_keyboard()
+
+    koos = randn(2,3)
+    sannie = 3
+    keyboard(@locals,@__FILE__,@__LINE__)
+
+
+
+end
+
+end # module
